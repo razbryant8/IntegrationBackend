730f1d5a16a25cf1fb662467cb106ea62d9cb084
omriO9 <omriohayon9@gmail.com>
2019-03-17 10:28:51 +0200

    test example with junit

---
+++ 2019B.zuru/src/main/test/data/UserEntityTest.java
@@ -0,0 +1,26 @@
+package data;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserEntityTest {
+
+    private UserEntity userEntity;
+
+    @Before
+    public void setUp(){
+        // Thanks to before annotation - this happens before every test
+        userEntity = new UserEntity();
+    }
+
+
+    // Test example
+    @Test
+    public void testSetPoints(){
+        userEntity.setPoints(20);
+
+        assertEquals("Wrong points value" , 20 , userEntity.getPoints());
+    }
+}
