3d252725d39a9601f898ba0e0a9232ce63b86253
Lenovo <binyamin.cohen111@gmail.com>
2019-03-26 17:09:03 +0200

    User Entity,Crud,Dao created.

--- 2019B.zuru/src/main/java/dao/rdb/ActionCrud.java
+++ 2019B.zuru/src/main/java/dao/rdb/ActionCrud.java
@@ -3,6 +3,8 @@ package dao.rdb;
 import data.ActionEntity;
 import org.springframework.data.repository.CrudRepository;
 
+
+
 public interface ActionCrud extends CrudRepository<ActionEntity, String> {
 
 }
