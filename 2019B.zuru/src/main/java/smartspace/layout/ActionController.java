93edb06e8f1c25422aa5f5f474274104594fc121
omriO9 <omriohayon9@gmail.com>
2019-04-22 20:57:00 +0300

    ActionController is almost done , to be continued

--- 2019B.zuru/src/main/java/smartspace/layout/ActionController.java
+++ 2019B.zuru/src/main/java/smartspace/layout/ActionController.java
@@ -2,10 +2,11 @@ package smartspace.layout;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import smartspace.logic.ActionService;
 
-import java.util.stream.Collectors;
+import java.util.Arrays;
 
 @RestController
 public class ActionController {
@@ -18,25 +19,42 @@ public class ActionController {
     }
 
 
-//    @RequestMapping(
-//            method = RequestMethod.GET,
-//            path = "/smartspace/admin/actions/{adminSmartspace}/{adminEmail}",
-//            produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ElementBoundary[] getAll(
-//            @PathVariable("adminSmartspace") String adminSmartspace,
-//            @PathVariable("adminEmail") String adminEmail,
-//            @RequestParam(name = "size", required = false, defaultValue = "10") int size,
-//            @RequestParam(name = "page", required = false, defaultValue = "0") int page) {
-//        if (validate(adminSmartspace, adminEmail)) {
-//            return this.actionService
-//                    .getAll(size, page)
-//                    .stream()
-//                    .map(ActionB::new)
-//                    .collect(Collectors.toList())
-//                    .toArray(new ElementBoundary[0]);
-//        } else
-//            throw new RuntimeException("Unauthorized operation");
-//    }
+    @RequestMapping(
+            method = RequestMethod.GET,
+            path = "/smartspace/admin/actions/{adminSmartspace}/{adminEmail}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ActionBoundary[] getAll(
+            @PathVariable("adminSmartspace") String adminSmartspace,
+            @PathVariable("adminEmail") String adminEmail,
+            @RequestParam(name = "size", required = false, defaultValue = "10") int size,
+            @RequestParam(name = "page", required = false, defaultValue = "0") int page) {
+        if (validate(adminSmartspace, adminEmail)) {
+            return this.actionService
+                    .getAll(size, page)
+                    .stream()
+                    .map(ActionBoundary::new).toArray(ActionBoundary[]::new);
+        } else
+            throw new RuntimeException("Unauthorized operation");
+    }
+
+
+    @Transactional
+    @RequestMapping(
+            method = RequestMethod.POST,
+            path = "/smartspace/admin/actions/{adminSmartspace}/{adminEmail}",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ActionBoundary[] store(
+            @PathVariable("adminSmartspace") String adminSmartspace,
+            @PathVariable("adminEmail") String adminEmail,
+            @RequestBody ActionBoundary[] actionBoundary) {
+        if (validate(adminSmartspace, adminEmail))
+            return Arrays.stream(actionBoundary).map(ActionBoundary::convertToEntity)
+                    .map(this.actionService::store)
+                    .map(ActionBoundary::new).toArray(ActionBoundary[]::new);
+        else
+            throw new RuntimeException("Unauthorized operation");
+    }
 
     private boolean validate(String adminSmartspace, String adminEmail) {
         return true;
