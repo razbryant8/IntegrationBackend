5d33df5ddac570f84c5e8db8965338dc59626455
MarkyL <mark.lu91@gmail.com>
2019-05-11 14:17:02 +0300

    - RdbElementDaoTest - spell check , refactor, code optimization

--- 2019B.zuru/src/test/smartspace/dao/rdb/RdbElementDaoTest.java
+++ 2019B.zuru/src/test/smartspace/dao/rdb/RdbElementDaoTest.java
@@ -14,10 +14,7 @@ import smartspace.data.ElementEntity;
 import smartspace.data.Location;
 import smartspace.data.util.EntityFactory;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -109,7 +106,7 @@ public class RdbElementDaoTest {
         // WHEN Read all rows
         List<ElementEntity> elementEntities = elementDao.readAll();
 
-        // THEN the list cnotains three elements and al of them are the same as above
+        // THEN the list contains three elements and al of them are the same as above
         assertEquals("Wrong row number", 3, elementEntities.size());
         assertThat(elementEntities).usingElementComparatorOnFields("key").contains(elementEntity1);
         assertThat(elementEntities).usingElementComparatorOnFields("key").contains(elementEntity2);
@@ -163,7 +160,7 @@ public class RdbElementDaoTest {
         // WHEN we delete entity
         elementDao.delete(elementEntity);
 
-        // THEN the entity does not exsist in the database
+        // THEN the entity does not exist in the database
         assertFalse("Failed deleting elementEntity by object", elementDao.readById(elementEntity.getElementId()).isPresent());
     }
 
@@ -214,7 +211,7 @@ public class RdbElementDaoTest {
                 .containsExactlyElementsOf(
                         actual
                                 .stream()
-                                .sorted((m1,m2)->m1.getKey().compareTo(m2.getKey()))
+                                .sorted(Comparator.comparing(ElementEntity::getKey))
                                 .limit(5)
                                 .collect(Collectors.toList()));
     }
@@ -237,7 +234,7 @@ public class RdbElementDaoTest {
     }
 
     @Test
-    public void testReadAllbyTypeUsingPaginationPageZeroSizeFive (){
+    public void testReadAllByTypeUsingPaginationPageZeroSizeFive (){
         // GIVEN the database contains 1 elements with type scooter
         String type = "scooter";
         enhancedDao.create(factory.createNewElement("name",type,new Location(5,4),new Date(),null,null,false,new HashMap<>()));
@@ -251,12 +248,12 @@ public class RdbElementDaoTest {
     }
 
     @Test
-    public void testReadAllbyTypeUsingPaginationPageZeroSize2 (){
+    public void testReadAllByTypeUsingPaginationPageZeroSize2 (){
         // GIVEN the database contains 2 elements with type scooter and one with another type
         String type = "scooter";
         ElementEntity entity1 = enhancedDao.create(factory.createNewElement("name",type,new Location(5,4),new Date(),null,null,false,new HashMap<>()));
         ElementEntity entity2 = enhancedDao.create(factory.createNewElement("name",type,new Location(5,4),new Date(),null,null,false,new HashMap<>()));
-        ElementEntity entity3 = enhancedDao.create(factory.createNewElement("name","blabla",new Location(5,4),new Date(),null,null,false,new HashMap<>()));
+        enhancedDao.create(factory.createNewElement("name","type",new Location(5,4),new Date(),null,null,false,new HashMap<>()));
 
         // WHEN we read up to 3 element by type from the beginning
         List<ElementEntity> actual = this.enhancedDao.getAllElementsByType(3, 0, type,"creationTimestamp");
@@ -269,7 +266,7 @@ public class RdbElementDaoTest {
     }
 
     @Test
-    public void testReadAllbyNameUsingPaginationPageZeroSizeFive (){
+    public void testReadAllByNameUsingPaginationPageZeroSizeFive (){
         // GIVEN the database contains 1 elements with type scooter
         String name = "name";
         enhancedDao.create(factory.createNewElement(name,null,new Location(5,4),new Date(),null,null,false,new HashMap<>()));
@@ -283,12 +280,12 @@ public class RdbElementDaoTest {
     }
 
     @Test
-    public void testReadAllbyNameUsingPaginationPageZeroSize2 (){
-        // GIVEN the database contains 2 elements with name nameand one with another type
+    public void testReadAllByNameUsingPaginationPageZeroSize2 (){
+        // GIVEN the database contains 2 elements with name name and one with another type
         String name = "name";
         ElementEntity entity1 = enhancedDao.create(factory.createNewElement(name,null,new Location(5,4),new Date(),null,null,false,new HashMap<>()));
         ElementEntity entity2 = enhancedDao.create(factory.createNewElement(name,null,new Location(5,4),new Date(),null,null,false,new HashMap<>()));
-        ElementEntity entity3 = enhancedDao.create(factory.createNewElement("Notname","blabla",new Location(5,4),new Date(),null,null,false,new HashMap<>()));
+        enhancedDao.create(factory.createNewElement("Not name","type",new Location(5,4),new Date(),null,null,false,new HashMap<>()));
 
         // WHEN we read up to 3 element by type from the beginning
         List<ElementEntity> actual = this.enhancedDao.getAllElementsByName(3, 0, name,"creationTimestamp");
@@ -305,7 +302,7 @@ public class RdbElementDaoTest {
         // GIVEN the database contains 3 elements within the specified location "rectangle"
         ElementEntity entity1 = enhancedDao.create(factory.createNewElement("mark1",null,new Location(50,50),new Date(),null,null,false,new HashMap<>()));
         ElementEntity entity2 = enhancedDao.create(factory.createNewElement("mark2",null,new Location(45,45),new Date(),null,null,false,new HashMap<>()));
-        ElementEntity entity3 = enhancedDao.create(factory.createNewElement("mark3","blabla",new Location(40,40),new Date(),null,null,false,new HashMap<>()));
+        ElementEntity entity3 = enhancedDao.create(factory.createNewElement("mark3","type",new Location(40,40),new Date(),null,null,false,new HashMap<>()));
 
         // WHEN we read all elements by location nearby to those 3
         List<ElementEntity> actual = this.enhancedDao.getAllElementsByLocation(3, 0, 46, 46, 10, "creationTimestamp");
@@ -322,7 +319,7 @@ public class RdbElementDaoTest {
         // GIVEN the database contains 3 elements within the specified location "rectangle"
         enhancedDao.create(factory.createNewElement("mark1",null,new Location(50,50),new Date(),null,null,false,new HashMap<>()));
         enhancedDao.create(factory.createNewElement("mark2",null,new Location(45,45),new Date(),null,null,false,new HashMap<>()));
-        enhancedDao.create(factory.createNewElement("mark3","blabla",new Location(40,40),new Date(),null,null,false,new HashMap<>()));
+        enhancedDao.create(factory.createNewElement("mark3","type",new Location(40,40),new Date(),null,null,false,new HashMap<>()));
 
         // WHEN we read all elements by location which are not near the specified "rectangle" radius.
         List<ElementEntity> actual = this.enhancedDao.getAllElementsByLocation(3, 0, 70, 70, 10, "creationTimestamp");
@@ -336,7 +333,7 @@ public class RdbElementDaoTest {
         // GIVEN the database contains 3 elements within the specified location "rectangle"
         ElementEntity entity1 = enhancedDao.create(factory.createNewElement("mark1",null,new Location(50,50),new Date(),null,null,false,new HashMap<>()));
         enhancedDao.create(factory.createNewElement("mark2",null,new Location(45,45),new Date(),null,null,false,new HashMap<>()));
-        enhancedDao.create(factory.createNewElement("mark3","blabla",new Location(40,40),new Date(),null,null,false,new HashMap<>()));
+        enhancedDao.create(factory.createNewElement("mark3","type",new Location(40,40),new Date(),null,null,false,new HashMap<>()));
 
         // WHEN we read all elements by location where only 1 of them is nearby the specified "rectangle".
         List<ElementEntity> actual = this.enhancedDao.getAllElementsByLocation(3, 0, 55, 55, 7, "creationTimestamp");
