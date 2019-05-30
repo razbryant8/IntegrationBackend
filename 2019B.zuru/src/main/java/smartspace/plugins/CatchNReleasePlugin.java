0e7cdb577cdd8a575879c3727dd9be4f08043030
MarkyL <mark.lu91@gmail.com>
2019-05-30 10:24:06 +0300

    initial commit for milestone 4

---
+++ 2019B.zuru/src/main/java/smartspace/plugins/CatchNReleasePlugin.java
@@ -0,0 +1,11 @@
+package smartspace.plugins;
+
+import smartspace.data.ActionEntity;
+
+public class CatchNReleasePlugin implements PluginCommand {
+
+    @Override
+    public ActionEntity execute(ActionEntity actionEntity) {
+        return null;
+    }
+}
