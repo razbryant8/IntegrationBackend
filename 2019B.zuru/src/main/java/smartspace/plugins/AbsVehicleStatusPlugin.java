7c7ca869f0d9751060409aca7788e72700fb2d0d
MarkyL <mark.lu91@gmail.com>
2019-05-31 12:08:03 +0300

    - Implementation of abstraction for the vehicle status classes - there is an AbsVehicleStatusPlugin - which is responsible for executing the status change of the vehicle. And there will be 4 different states which are covered in 3 different plugins - CatchNRelease , ReportVehicleLowBattery, ReportVehicleMalfunction.

--- 2019B.zuru/src/main/java/smartspace/plugins/AbsVehicleStatusPlugin.java
+++ 2019B.zuru/src/main/java/smartspace/plugins/AbsVehicleStatusPlugin.java
@@ -1,8 +1,6 @@
 package smartspace.plugins;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import smartspace.data.ActionEntity;
 import smartspace.data.ElementEntity;
 import smartspace.data.UserRole;
@@ -10,19 +8,11 @@ import smartspace.data.VehicleStatus;
 import smartspace.logic.ElementService;
 
 import java.util.HashMap;
-import java.util.Map;
 
-@Component
-public class ReportVehicleStatusPlugin implements PluginCommand{
+public abstract class AbsVehicleStatusPlugin implements PluginCommand {
 
-    private ObjectMapper jackson;
-    private ElementService elementService;
-
-    @Autowired
-    public ReportVehicleStatusPlugin(ElementService elementService) {
-        this.elementService = elementService;
-        this.jackson = new ObjectMapper();
-    }
+    protected ObjectMapper jackson;
+    protected ElementService elementService;
 
     @Override
     public ActionEntity execute(ActionEntity actionEntity) {
